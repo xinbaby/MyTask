ca680d77a7418224c9c1dc0af0c4c0119f18e2fd
xinbaby <109004021@qq.com>
2016-06-15 17:35:43 +0800

    add file

--- MyTask/app/src/main/java/sharpmetro/site/mytask/views/QuizActivity.java
+++ MyTask/app/src/main/java/sharpmetro/site/mytask/views/QuizActivity.java
@@ -8,7 +8,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz)
+        setContentView(R.layout.activity_quiz);
 
     }
 
